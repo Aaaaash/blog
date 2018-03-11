9beadcd7a5f6977985b70d277b8e5fecdaceacb2
SakuraAsh <841105127@qq.com>
2018-03-11 12:05:34 +0800

    add LinkedList

---
+++ src/LinkedList/index.ts
@@ -0,0 +1,16 @@
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+  }
+  public append(element: Node) {}
+  public insert(position: number, element: Node) {}
+  public removeAt(position: number){}
+  public remove(element: Node){}
+  public indexOf(element: Node){}
+  public isEmpty(): boolean{}
+  public size(): number{}
+  public getHead(): Node{}
+  public toString(): string {}
+  public print() {}
+}
